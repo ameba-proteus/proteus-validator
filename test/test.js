8759fd6ffd050ab388a2a0e419739dce5b1def1f
matsukaz <matsukaz@gmail.com>
2012-12-05 19:58:03 +0900

    implemented validation methods

--- test/test.js
+++ test/test.js
@@ -23,16 +23,76 @@ var schema = {
         favoriteFiveLetterWord          : { type : "string", required : true, minLength : 5, maxLength : 5 },
         emailAddresses                  : { type : "array", required : true, minItems : 1, uniqueItems : true, items : { type : "string", format : "email" }},
         ipAddresses                     : { type : "array", required : true, uniqueItems : true, items : { type : "string", format : "ip-address" }},
+        favorite                        : { type : "object", required: true, properties : {
+                                                  date : { type : "string", required : true },
+                                                  site : { type : "string", required : true, format: "uri" }
+                                              }
+                                          },
+        hoge                            : { type : "string", fuga : "foo" }
     }
 }
 
-console.log('error : ' + JSON.stringify(Validator.validateSchema(schema)));
+Validator.validateSchema(schema, function(err) {
+	console.log('error : ' + JSON.stringify(err, null, ' '));
+});
 
-var schema2 = {
-	name : 'test2',
-	type : 'string',
-	required: true
+var instance = {
+    fullName : "John Doe",
+    age : 47,
+    state : "Massachusetts",
+    city : "Boston",
+    zip : 02201,
+    married : false,
+    dozen : 12,
+    dozenOrBakersDozen : 13,
+    favoriteEvenNumber : 14,
+    topThreeFavoriteColors : [ "red", "purple", "lime" ],
+    favoriteSingleDigitWholeNumbers : [ 7 ],
+    favoriteFiveLetterWord : "coder",
+    emailAddresses :
+    [
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
+//        "01234567890@numbers-in-local.net",
+//        "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
+//        "mixed-1234-in-{+^}-local@sld.net",
+//        "a@single-character-in-local.org",
+//        "\"quoted\"@sld.com",
+//        "\"\\e\\s\\c\\a\\p\\e\\d\"@sld.com",
+//        "\"quoted-at-sign@sld.org\"@sld.com",
+//        "\"escaped\\\"quote\"@sld.com",
+//        "\"back\\slash\"@sld.com",
+//        "one-character-third-level@a.example.com",
+//        "single-character-in-sld@x.org",
+//        "local@dash-in-sld.com",
+//        "letters-in-sld@123.com",
+//        "one-letter-sld@x.org",
+//        "uncommon-tld@sld.museum",
+//        "uncommon-tld@sld.travel",
+//        "uncommon-tld@sld.mobi",
+//        "country-code-tld@sld.uk",
+//        "country-code-tld@sld.rw",
+//        "local@sld.newTLD",
+//        "the-total-length@of-an-entire-address.cannot-be-longer-than-two-hundred-and-fifty-four-characters.and-this-address-is-254-characters-exactly.so-it-should-be-valid.and-im-going-to-add-some-more-words-here.to-increase-the-lenght-blah-blah-blah-blah-bla.org",
+//        "the-character-limit@for-each-part.of-the-domain.is-sixty-three-characters.this-is-exactly-sixty-three-characters-so-it-is-valid-blah-blah.com",
+        "local@sub.domains.com"
+    ],
+    ipAddresses : [ "127.0.0.1", "24.48.64.2", "192.168.1.1", "209.68.44.3", "2.2.2.2" ],
+    favorite : {
+        date : 'a',
+        site : "http://www.facebook.com/",
+    },
+    hoge : 'foo'
 }
 
-console.log('error : ' + JSON.stringify(Validator.validateSchema(schema2)));
+Validator.addValidation('fuga', function(schema, instance) {
+	if (instance !== schema.fuga) {
+		return 'value must be \'' + schema.fuga + '\'';
+	}
+	return;
+});
+
+Validator.validate(schema, instance, function(err) {
+	console.log('error : ' + JSON.stringify(err, null, ' '));
+});
+
 
